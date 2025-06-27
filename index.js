70f2e3d23310089e8e6af7309c9d4e9a5687b18d
saikirankathoju <saikiran.katoju@empover.com>
2025-06-27 17:15:41 +0530

    changed the component name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import LoaderScreen from "./src/classes/LoaderScreen"
+import WeatherLoaderScreen from "./src/classes/WeatherLoaderScreen"
 import WeatherScreen from "./src/classes/Weather/WeatherScreen"
 import Remedyrecommendation from "./src/classes/Weather/Remedyrecommendation"
 import Location from "./src/components/Location"
 
-export { LoaderScreen,WeatherScreen, Remedyrecommendation, Location }
+export { WeatherLoaderScreen,WeatherScreen, Remedyrecommendation, Location }
